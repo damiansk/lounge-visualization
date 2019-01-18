63b429ea48f64c4ac5c92f6253c90f552fd28b78
dstolarek <dstolarek@pgs-soft.com>
2019-01-18 10:17:33 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,26 @@
 module.exports = {
-    root: true,
-    extends: 'airbnb',
-    plugins: ['react', 'jsx-a11y', 'import'],
-    env: {
-      browser: true,
-      es6: true,
-    },
-    parser: 'babel-eslint',
-    rules: {
-      'no-plusplus': 'off',
-      'default-case': 'off',
-      'consistent-return': 'off',
-      'class-methods-use-this': 'off',
-      'one-var': [
-        'error',
-        {
-          initialized: 'never',
-        },
-      ],
-      'one-var-declaration-per-line': ['error', 'initializations'],
-      'react/require-default-props': 'off',
-      'react/jsx-filename-extension': [
-        'error',
-        {
-          extensions: ['.js'],
-        },
-      ],
-      'react/jsx-tag-spacing': [
-        'error',
-        {
-          closingSlash: 'never',
-          beforeSelfClosing: 'allow',
-          afterOpening: 'never',
-        },
-      ],
-      'jsx-a11y/href-no-hash': 'off',
-      'jsx-a11y/anchor-is-valid': ['warn', { aspects: ['invalidHref'] }],
-    },
-  };
+  root: true,
+  extends: ['airbnb-base', 'plugin:react/recommended', 'react-app', 'prettier'],
+  plugins: ['import'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  parser: 'babel-eslint',
+  rules: {
+    'no-plusplus': 'off',
+    'default-case': 'off',
+    'consistent-return': 'off',
+    'class-methods-use-this': 'off',
+    'one-var': [
+      'error',
+      {
+        initialized: 'never',
+      },
+    ],
+    'one-var-declaration-per-line': ['error', 'initializations'],
+    'linebreak-style': ["error", "windows"]
+  },
+};
