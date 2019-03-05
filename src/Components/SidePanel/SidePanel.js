34927869a5fb8d7702c09ffed1fdb8f123a8ef6f
maxuser <mrachkovskiy@pgs-soft.com>
2019-03-05 19:56:26 +0100

    JSON-export: drawer panel as function component, header styles.

--- src/Components/SidePanel/SidePanel.js
+++ src/Components/SidePanel/SidePanel.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Drawer, Divider, IconButton } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
@@ -9,44 +9,34 @@ import { ModelsList } from '../ModelsList';
 import { JSONExportButton } from '../JSONExportButton';
 import { styles } from './styles';
 
-class DrawerPanel extends Component {
-  render() {
-    const {
-      classes,
-      theme,
-      open,
-      handleDrawerClose,
-      store,
-      handleExportButtonClick,
-    } = this.props;
-
-    return (
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        <JSONExportButton handleExportButtonClick={handleExportButtonClick} />
-        <Divider />
-        <ModelsList store={store} />
-      </Drawer>
-    );
-  }
-}
+const DrawerPanel = ({
+  classes,
+  theme,
+  open,
+  handleDrawerClose,
+  store,
+  handleExportButtonClick,
+}) => (
+  <Drawer
+    className={classes.drawer}
+    variant="persistent"
+    anchor="left"
+    open={open}
+    classes={{
+      paper: classes.drawerPaper,
+    }}
+  >
+    <div className={classes.drawerHeader}>
+      <IconButton onClick={handleDrawerClose}>
+        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+      </IconButton>
+    </div>
+    <Divider />
+    <JSONExportButton handleExportButtonClick={handleExportButtonClick} />
+    <Divider />
+    <ModelsList store={store} />
+  </Drawer>
+);
 
 DrawerPanel.propTypes = {
   store: PropTypes.instanceOf(ModelsStore),
