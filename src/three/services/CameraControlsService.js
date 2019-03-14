8f358ee5ec4b4491a42d6f929cfae2f0435195b9
bumpMind <damian.s.ja@gmail.com>
2019-03-14 18:12:17 +0100

    Add local version of OrbitControls and limit scope of camera manipulation

--- src/three/services/CameraControlsService.js
+++ src/three/services/CameraControlsService.js
@@ -1,12 +1,18 @@
-import OrbitControls from 'orbit-controls-es6';
+import { OrbitControls } from '../libs/orbit-controls';
 
 let controls = null;
 
 class CameraControlsService {
   static init(camera, domElement) {
     controls = new OrbitControls(camera, domElement);
-    controls.enabled = true;
 
+    controls.enabled = true;
+    controls.minDistance = 10;
+    controls.maxDistance = 40;
+    controls.minPolarAngle = 0;
+    controls.maxPolarAngle = Math.PI/2;
+    controls.panningMode  = Math.PI/2;
+    
     return controls;
   }
 
