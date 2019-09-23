34b3113c8ad1d207edb101ccea1bfc73f5236155
bumpMind <damian.s.ja@gmail.com>
2019-09-23 18:08:01 +0200

    Handle remove model event

--- src/three/store/ModelsStore.js
+++ src/three/store/ModelsStore.js
@@ -61,6 +61,7 @@ class ModelsStore {
     this.models = this.models.filter(m => !model.isEqual(m));
     this.updateModelsSubject$.next(this.models);
     this.removeModelsSubject$.next(model);
+    model.destroy();
   }
 
   changeName(model, newName) {
